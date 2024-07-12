426d9277bc376436a2df8234e65594e3fb08e152
binseoyun <167400397+binseoyun@users.noreply.github.com>
2024-07-12 19:38:29 +0900

    Add files via upload

---
+++ SUMMER.Lab02_2.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main() {
+    long A, B, C;
+    scanf_s("%ld\n", &A);
+    scanf_s("%ld\n", &B);
+    scanf_s("%ld\n", &C);
+
+    printf("%ld", A + B + C);
+    return 0;
+}
