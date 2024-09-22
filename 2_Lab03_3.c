fe09e1d133b8af82fc14b5e35d2733547b1fced9
binseoyun <167400397+binseoyun@users.noreply.github.com>
2024-09-22 23:29:50 +0900

    2_Lab03

---
+++ 2_Lab03_3.c
@@ -0,0 +1,25 @@
+/*
+//2460번
+#include<stdio.h>
+int main() {
+	int in[10];
+	int out[10];
+	for (int i = 0; i < 10; i++) { //탄사람 내린사람 입력받음
+		scanf("%d %d", &out[i], &in[i]);
+	}
+
+	int sum = 0;
+	int count[10];
+	int max = sum;
+	for (int i = 0; i < 10; i++) {
+		sum += in[i];
+		sum -= out[i];
+		
+		if (sum > max) {
+			max = sum;
+		}
+
+	}
+	printf("%d", max);
+
+}*/
